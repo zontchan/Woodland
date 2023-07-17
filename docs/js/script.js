53e5207a196290130336f2110aeb79918c70d3ef
zontchan <semenovajuls@icloud.com>
2023-07-17 18:29:43 +0700

    edited

--- docs/js/script.js
+++ docs/js/script.js
@@ -15,9 +15,6 @@ headerBurger();
 headerSearch();
 /*Popup search start*/
 
-var ghpages = require('gh-pages');
-
-ghpages.publish('dist', function(err) {});
 
 /*Company info section start*/
 goSlider();
